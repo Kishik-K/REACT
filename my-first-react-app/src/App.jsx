d96a689ad3ceb8c45e736fa0cca42ddc5bc95aa4
Kishik-K <kishikkhajuria03@gmail.com>
2026-07-19 23:13:09 +0530

    imported spinner

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/search";
+import Spinner from "./components/spinner";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -65,10 +66,10 @@ function App() {
         </header>
 
         <section className="all-movies">
-          <h2> All movies</h2>
+          <h2 className="mt-[30px]"> All movies</h2>
 
           {isLoading ? (
-            <p className="text-white">Loading...</p>
+            <Spinner />
           ) : errorMessage ? (
             <p className="text-red-500">{errorMessage}</p>
           ) : (
