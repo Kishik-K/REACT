f1f05944d795e49c959677a76bf5df495c564974
Kishik-K <kishikkhajuria03@gmail.com>
2026-07-19 15:39:45 +0530

    importing tailwind.css

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,44 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title}, Has been liked: ${hasLiked}`);
-  }, [count, hasLiked]);
-
-  useEffect(() => {
-    console.log("Card Rendered");
-  }, []);
-
+function App() {
   return (
-    <div
-      className="card"
-      onClick={() => {
-        setCount((value) => value + 1);
-      }}
-    >
-      <h2>
-        {title} <br />
-        {count || null}
-      </h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "Liked" : "Like"}
-      </button>
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
-};
-
-const App = () => {
-  return (
-    <div className="app">
-      <Card title="STAR WARS" />
-      <Card title="THE LORD OF THE RINGS" />
-      <Card title="THE HUNGER GAMES" />
-    </div>
-  );
-};
+}
 
 export default App;
