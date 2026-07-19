39370acf1351fd8c75cc3ddc1e4c14ed1f2ad4dd
Kishik-K <kishikkhajuria03@gmail.com>
2026-07-19 23:14:56 +0530

    importing movieCard

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/search";
 import Spinner from "./components/spinner";
+import movieCard from "./components/movieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
