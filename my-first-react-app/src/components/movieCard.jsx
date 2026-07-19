5eff0d98a580e0e9784e54107413fa371edee183
Kishik-K <kishikkhajuria03@gmail.com>
2026-07-19 23:32:28 +0530

    cards image added

--- my-first-react-app/src/components/movieCard.jsx
+++ my-first-react-app/src/components/movieCard.jsx
@@ -4,8 +4,15 @@ function MovieCard({
   movie: { title, vote_average, poster_path, release_date, original_language },
 }) {
   return (
-    <div>
-      <p className="text-white">{title}</p>
+    <div className="movie-card">
+      <img
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500${poster_path}`
+            : "/no-movie.jpg"
+        }
+        alt={title}
+      />
     </div>
   );
 }
