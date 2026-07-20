7aad8b60780828dae3615c282326d57ebb801fb8
Kishik-K <kishikkhajuria03@gmail.com>
2026-07-21 00:03:39 +0530

    implementing trending

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -90,8 +90,22 @@ function App() {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
         <section className="all-movies">
-          <h2 className="mt-[30px]"> All movies</h2>
+          <h2> All movies</h2>
 
           {isLoading ? (
             <Spinner />
